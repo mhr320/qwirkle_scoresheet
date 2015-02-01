07ca147a02aec8f89b1e274584ac162b12b8e807
michael roberts <mhr320@gmail.com>
2015-02-01 12:45:01 -0500

    Just getting started...

--- qwirkle_scoresheet.py
+++ qwirkle_scoresheet.py
@@ -5,8 +5,11 @@
 # Collaborators: None
 # Time: 
 ################################################
-
+i = 1
 player1 = raw_input("Player1's name: ")
 player2 = raw_input("Player2's name: ")
 
+while 1:
+	player1Score = 0
+	player2Score = 0
 
