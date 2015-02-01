645c2fc8000bac323abc9744203e925eb36eefec
michael roberts <mhr320@gmail.com>
2015-02-01 12:38:16 -0500

    Opening program file

---
+++ qwirkle_scoresheet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+################################################
+# Problem Set 
+# Name: Michael H. Roberts
+# Collaborators: None
+# Time: 
+################################################
+
+player1 = raw_input("Player1's name: ")
+player2 = raw_input("Player2's name: ")
+
+
