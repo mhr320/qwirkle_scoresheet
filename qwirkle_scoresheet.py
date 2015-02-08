4a46ad2fa22b14e667e171fb03f58b207b62962c
michael roberts <mhr320@gmail.com>
2015-02-08 12:49:52 -0500

    scripting the idea

--- qwirkle_scoresheet.py
+++ qwirkle_scoresheet.py
@@ -5,11 +5,22 @@
 # Collaborators: None
 # Time: 
 ################################################
-i = 1
-player1 = raw_input("Player1's name: ")
-player2 = raw_input("Player2's name: ")
+import sys
 
+i = 1 
+scoret = []
+scored = []
+score_int = []
+score_int2 = []
 while 1:
-	player1Score = 0
-	player2Score = 0
-
+	i = raw_input('Enter Score >>>  ')
+	if i in ('t'):
+		scoret.append(i)
+	if i in ('d'):
+		scored.append(i)
+for i in scoret:
+	score_int.append(int(i))
+	x = sum(score_int)
+for i in scored:
+	score_int2.append(int(i))
+	y = sum(score_int2)
