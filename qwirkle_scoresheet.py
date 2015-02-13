642a7125727031545c6911850020f22956263603
michael roberts <mhr320@gmail.com>
2015-02-13 10:38:55 -0500

    have in loop, ready for some fine tuning

--- qwirkle_scoresheet.py
+++ qwirkle_scoresheet.py
@@ -9,31 +9,29 @@ import sys
 from RobFuns import *
 
 i = 1
+p2 = 'None'
 s1 = []
 ss1 = []
 s2 = []
 ss2 = []
 
-player1 = str(raw_input("Please enter Player 1 >>> "))
-player2 = str(raw_input("Please enter Player 2 >>> "))
+player1 = str(raw_input("Please enter Player 1 >>> \n"))
+player2 = str(raw_input("Please enter Player 2 >>> \n"))
 
 
 yana("Do you wish to start the game")
 
-
-if yana:
-	yana("%s Ready" % player1)
+while yana:
+	yana("%s Ready \n" % player1)
 	if yana:
 		scorer(s1,ss1)
+	p1 = sum(ss1)
+	print "%s's score: %s & %s's score: %s \n" % (player1, p1, player2, p2)
 
-p1 = sum(ss1)
-print "%s's score: %s \n" % (player1, p1)
+	yana("%s Ready \n" % player2)
 
-yana("%s Ready" % player2)
+  	scorer(s2,ss2)
 
-if yana:
-  scorer(s2,ss2)
-
-p2 = sum(ss2)
-print "%s's score: %s" % (player2, p2)
+	p2 = sum(ss2)
+	print "%s's score: %s & %s's score: %s \n" % (player1, p1, player2, p2)
 
